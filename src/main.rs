a3ab240aa463fc813eb67df71bf4cb22be59d1c6
nikicivan <ivan.nikic@gmail.com>
2024-08-07 19:49:26 +0200

    updated expected command

--- src/main.rs
+++ src/main.rs
@@ -39,7 +39,7 @@ async fn handle_conn(stream: TcpStream) {
         let response = if let Some(v) = value {
             let (command, args) = extract_command(v).unwrap();
             match command.as_str() {
-                "ping" => Value::SimpleString("PONG".to_string()),
+                "PING" => Value::SimpleString("PONG".to_string()),
                 "ECHO" => args.first().unwrap().clone(),
                 _ => panic!("Cannot handle command {}", command),
             }
